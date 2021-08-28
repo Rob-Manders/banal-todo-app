0ea4b90ae0b78f000b907c498fa48e2611e22c7a
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2021-08-28 14:28:45 +0100

    Fonts / Absolute path aliases

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,22 @@ module.exports = {
 					'css-loader',
 					'sass-loader'
 				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
 			}
 		]
 	},
 	resolve: {
-		extensions: ['*', '.js', '.jsx']
+		extensions: ['*', '.js', '.jsx'],
+		alias: {
+			'@Components': path.resolve(__dirname, 'src/components/'),
+			'@Fonts': path.resolve(__dirname, 'src/fonts/'),
+			'@Pages': path.resolve(__dirname, 'src/pages/'),
+			'@Source': path.resolve(__dirname, 'src/'),
+			'@Styles': path.resolve(__dirname, 'src/scss/')
+		}
 	},
 	output: {
 		filename: 'main.js',
